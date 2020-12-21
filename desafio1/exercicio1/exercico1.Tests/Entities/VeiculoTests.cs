f0965bd54c0652d90110d62f2bb85bce41b15b39
foeu <fabiano.junior@gft.com>
2020-12-21 14:47:17 -0300

    Implementados casos de teste para entidade veiculo

--- desafio1/exercicio1/exercico1.Tests/Entities/VeiculoTests.cs
+++ desafio1/exercicio1/exercico1.Tests/Entities/VeiculoTests.cs
@@ -11,6 +11,33 @@ namespace exercico1.Tests.Entities
 
         [TestMethod]
         [TestCategory("Domain")]
+        public void LigarOVeiculoDesligado()
+        {
+            _veiculo.Ligar();
+            Assert.AreEqual(_veiculo.IsLigado, EVeiculoIsLigado.Ligado);
+        }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void DesligarVeiculoLigadoComVelocidadeIgualZero()
+        {
+            _veiculo.Ligar();
+            _veiculo.Desligar();
+            Assert.AreEqual(_veiculo.IsLigado, EVeiculoIsLigado.Desligado);
+        }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void DesligarVeiculoLigadoComVelocidadeMaiorZero()
+        {
+            _veiculo.Ligar();
+            _veiculo.Acelerar();
+            _veiculo.Desligar();
+            Assert.AreEqual(_veiculo.IsLigado, EVeiculoIsLigado.Ligado);
+        }
+
+        [TestMethod]
+        [TestCategory("Domain")]
         public void AcelerarComVeiculoLigado()
         {
             int velocidade = _veiculo.Velocidade;
@@ -26,6 +53,81 @@ namespace exercico1.Tests.Entities
             int velocidade = _veiculo.Velocidade;
             _veiculo.Acelerar();
             Assert.AreEqual(_veiculo.Velocidade, velocidade);
+        }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void FrearVeiculoLigadoComVelocidadeMaiorZero()
+        {
+            _veiculo.Ligar();
+            _veiculo.Acelerar();
+            _veiculo.Frear();
+
+            Assert.AreEqual(_veiculo.Velocidade, 0);
+        }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void FrearVeiculoLigadoComVelocidadeIgualZero()
+        {
+            _veiculo.Ligar();
+            _veiculo.Frear();
+            Assert.AreEqual(_veiculo.Velocidade, 0);
+        }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void PintarVeiculoDeVermelho()
+        {
+            string cor = "Vermelho";
+            _veiculo.Pintar(cor);
+            Assert.AreEqual(_veiculo.Cor, cor);
+        }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void PintarVeiculoSemPassarCor()
+        {
+            _veiculo.Pintar(null);
+            Assert.AreEqual(_veiculo.Cor, "Branco");
+        }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void AbastecerVeiculoDesligadoCom10LitrosCombustivel()
+        {
+            _veiculo.Abastecer(20);
+            Assert.AreEqual(_veiculo.LitrosCombustivel, 30);
+        }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void AbastecerVeiculoLigadoCom10LitrosCombustivel()
+        {
+            _veiculo.Ligar();
+            _veiculo.Abastecer(20);
+            Assert.AreEqual(_veiculo.LitrosCombustivel, 10);
+        }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void AbastecerVeiculoDesligadoCom60LitrosCombustivel()
+        {
+            Veiculo veiculo = new Veiculo("Chevrolet", "Onix", "ETZ-4310", "Branco", 15000, 60, 75000);
+
+            veiculo.Abastecer(20);
+            Assert.AreEqual(veiculo.LitrosCombustivel, 60);
+        }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void AbastecerVeiculoLigadoCom60LitrosCombustivel()
+        {
+            Veiculo veiculo = new Veiculo("Chevrolet", "Onix", "ETZ-4310", "Branco", 15000, 60, 75000);
+
+            veiculo.Ligar();
+            veiculo.Abastecer(20);
+            Assert.AreEqual(veiculo.LitrosCombustivel, 60);
         }
     }
 }
