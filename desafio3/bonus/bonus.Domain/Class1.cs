3b47a6f7b439b5654afd7ee50936ceefbe1a9a4a
foeu <fabiano.junior@gft.com>
2020-12-23 14:49:55 -0300

    Subindo projeto

---
+++ desafio3/bonus/bonus.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace bonus.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ desafio3/bonus/bonus.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace bonus.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
